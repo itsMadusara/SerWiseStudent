1d789c4a25647d3f950bae80e143a214c8733e47
Tharushka Madusara <tharushka.madusara1234@gmail.com>
2023-05-14 22:52:23 +0530

    Profile changes and reason to put the project in C

--- src/main/java/com/CS01/SerWise/Services/Profile/ServletGenerateOTPClient.java
+++ src/main/java/com/CS01/SerWise/Services/Profile/ServletGenerateOTPClient.java
@@ -7,6 +7,7 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
+@WebServlet(name = "ServletGenerateOTPClient", value = "/ServletGenerateOTPClient")
 public class ServletGenerateOTPClient extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -27,5 +28,6 @@ public class ServletGenerateOTPClient extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 }
